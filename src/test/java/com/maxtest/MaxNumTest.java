3ce9a8191b4d72d6b5b754be51a3627b3d8b0a78
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-02 00:40:00 +0530

    find max string

--- src/test/java/com/maxtest/MaxNumTest.java
+++ src/test/java/com/maxtest/MaxNumTest.java
@@ -12,13 +12,11 @@ public class MaxNumTest {
         Integer maximumNum = maxNum.testMax(4, 5, 3);
         Assertions.assertEquals(4, maximumNum);
     }
-
     @Test
     public void givenMaxIntegerNumAtSecondPosition_ShouldReturnSameNum() {
         Integer maximumNum = maxNum.testMax(4, 5, 3);
         Assertions.assertEquals(5, maximumNum);
     }
-
     @Test
     public void givenMaxIntegerNumAtThirdPosition_ShouldReturnSameNum() {
         Integer maximumNum = maxNum.testMax(4, 5, 3);
@@ -31,7 +29,6 @@ public class MaxNumTest {
         Float maximumNum = maxNum.testMax(10.5f, 12.6f, 13.01f);
         Assertions.assertEquals(10.5f, maximumNum);
     }
-
     @Test
     public void givenMaxFloatNumAtSecondPosition_ShouldReturnSameNum() {
         Float maximumNum = maxNum.testMax(10.5f, 12.6f, 13.01f);
@@ -41,5 +38,22 @@ public class MaxNumTest {
     public void givenMaxFloatNumAtThirdPosition_ShouldReturnSameNum() {
         Float maximumNum = maxNum.testMax(10.5f, 12.6f, 13.01f);
         Assertions.assertEquals(13.01f, maximumNum);
+    }
+
+    // Test cases to find maximum String.
+    @Test
+    public void givenMaxStringAtFirstPosition_ShouldReturnSameNum() {
+        String maximumNum = maxNum.testMax("Apple", "Peach", "Banana");
+        Assertions.assertEquals("Apple", maximumNum);
+    }
+    @Test
+    public void givenMaxStringAtSecondPosition_ShouldReturnSameNum() {
+        String maximumNum = maxNum.testMax("Apple", "Peach", "Banana");
+        Assertions.assertEquals("Peach", maximumNum);
+    }
+    @Test
+    public void givenMaxStringAtThirdPosition_ShouldReturnSameNum() {
+        String maximumNum = maxNum.testMax("Apple", "Peach", "Banana");
+        Assertions.assertEquals("Banana", maximumNum);
     }
 }
